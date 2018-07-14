2817cb223c507d0234b9ac2953008826762f9e01
Broodis_Desktop <j.porter712@gmail.com>
2018-07-14 12:57:30 -0700

    remote

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,9 @@
 var topics = ['PubG', 'Fortnite', 'Super Smash Bros', 'Overwatch', 'Far Cry 5', 'League of Legends', 'World of Warcraft',]
-
+var stillImage = '';
+var animatedImage = '';
+var gifStatus = '';
+var stillUrl = '';
+var animatedUrl = '';
 var $buttonArea = $('#buttonCol');
 
 /* Functions:
@@ -34,12 +38,53 @@ var addButton = function () {
     topics.push(input);
     createBtn();
 }
-var displayGif = function() {
+var displayGif = function () {
     var btnTpc = $(this).data("name");
     var apiKey = "unE0UiNlYcS1P3CH0M8fUhjUQBxlkgPk";
     var apiUrl = "https://api.giphy.com/v1/gifs/search?q=" + btnTpc + "&api_key=" + apiKey;
+    $.ajax({
+        url: apiUrl,
+        method: 'GET'
+    }).done(function (response) {
+        $('.gifImageRow').empty();
+        let newH1 = $('<h1>');
+        newH1.html(btnTpc);
+        $('.gifRow').empty();
+        $('.gifRow').append(newH1);
+
+        for (var i = 0; i < 10; i++) {
+            stillImage = response['data'][i]['images']['fixed_height_still']['url'];
+            animatedImage = response['data'][i]['images']['fixed_height']['url'];
+            var rating = response['data'][i]['rating'];
+            var newDiv = $('<div class="col-sm-12 col-lg-3">');
+            var newP = $('<p>');
+            var newImg = $('<img>');
+            newImg.attr('data-still', stillImage);
+            newImg.attr('data-animate', animatedImage);
+            newImg.attr('src', stillImage);
+            newImg.attr('data-type', 'still');
+            newImg.addClass('gifImage');
+            newP.html('Giphy Rating: ' + rating);
+            $(newP).appendTo(newDiv)
+            $(newImg).appendTo(newDiv);
+            $('.gifImageRow').append(newDiv);
+        }
+    });
+}
+var startGif = function () {
+    gifStatus = $(this).data('type');
+    stillUrl = $(this).data('still');
+    animatedUrl = $(this).data('animate');
+    if (gifStatus === 'still') {
+        $(this).attr('src', animatedUrl);
+        $(this).data('type', 'animate');
+    } else if (gifStatus === 'animate') {
+        $(this).attr('src', stillUrl);
+        $(this).data('type', 'still');
+    }
 }
 
 /* Main calls */
 createBtn();
 $(document).on('click', '.buttons', displayGif);
+$(document).on('click', '.gifImage', startGif);
